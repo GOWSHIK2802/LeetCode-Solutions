50a898150acd9f456982f6378ef1f95845e729e5
GOWSHIK2802 <gowshikannamalai2007@gmail.com>
2026-08-18 22:43:07 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (29.29%) - LeetSync

---
+++ 62-unique-paths/unique-paths.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int uniquePaths(int m, int n) {
+        
+        int[][] memo = new int[m][n];
+        
+        
+        for (int i = 0; i < m; i++) {
+            Arrays.fill(memo[i], -1);
+        }
+        
+        
+        return uniquePathsRecursive(0, 0, m, n, memo);
+    }
+    
+    public int uniquePathsRecursive(int x, int y, int m, int n, int[][] memo) {
+        
+        if (x == m - 1 && y == n - 1) {
+            return 1;
+        }
+        
+        
+        if (memo[x][y] != -1) {
+            return memo[x][y];
+        }
+        
+        
+        int rightPaths = 0;
+        int downPaths = 0;
+        
+        
+        if (x < m - 1) {
+            rightPaths = uniquePathsRecursive(x + 1, y, m, n, memo);
+        }
+        
+        
+        if (y < n - 1) {
+            downPaths = uniquePathsRecursive(x, y + 1, m, n, memo);
+        }
+        
+        
+        memo[x][y] = rightPaths + downPaths;
+        return memo[x][y];
+    }
+}
