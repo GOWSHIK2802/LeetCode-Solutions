9ccb7ff3bd3542f62fe82aa86ba6362890a745d9
GOWSHIK2802 <gowshikannamalai2007@gmail.com>
2026-09-24 23:33:36 +0530

    Time: 10 ms (97.26%) | Memory: 47.9 MB (49.32%) - LeetSync

---
+++ 1113-moving-stones-until-consecutive-ii/moving-stones-until-consecutive-ii.java
@@ -0,0 +1,26 @@
+
+class Solution {
+    public int[] numMovesStonesII(int[] stones) {
+        Arrays.sort(stones);
+        int n = stones.length;
+        int maxMove = Math.max(stones[n - 1] - stones[1] + 1 - (n - 1),
+                               stones[n - 2] - stones[0] + 1 - (n - 1));
+
+        int minMove = n;
+        int i = 0;
+
+        for (int j = 0; j < n; ++j) {
+            
+            while (stones[j] - stones[i] + 1 > n) i++;
+            int alreadyPlaced = j - i + 1;
+
+            
+            if (alreadyPlaced == n - 1 && stones[j] - stones[i] + 1 == n - 1)
+                minMove = Math.min(minMove, 2);
+            else
+                minMove = Math.min(minMove, n - alreadyPlaced);
+        }
+
+        return new int[]{minMove, maxMove};
+    }
+}
